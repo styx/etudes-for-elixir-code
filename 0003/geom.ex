0b3fcabfa63ae78b3948873079eb2af7a117ea9a
Mikhail S. Pobolovets <styx.mp@gmail.com>
2014-01-31 21:23:40 +0300

    Etudes 3-2 and 3-3
    Signed-off-by: Mikhail S. Pobolovets <styx.mp@gmail.com>

--- 0003/geom.ex
+++ 0003/geom.ex
@@ -18,19 +18,24 @@ defmodule Geom do
     7.5
     iex(4)> Geom.area(:ellipse, 2, 4)
     25.132741228718345
-
+    iex(6)> Geom.area(:triangle, 3, -4)
+    ** (FunctionClauseError) no function clause matching in Geom.area/3
   """
   @spec area(:atom, number, number) :: number
-  def area(:rectangle, a, b) do
+  def area(:rectangle, a, b) when a >= 0 and b >= 0 do
     a * b
   end
 
-  def area(:triangle, a, b) do
+  def area(:triangle, a, b) when a >= 0 and b >= 0  do
     a * b / 2
   end
 
-  def area(:ellipse, a, b) do
+  def area(:ellipse, a, b) when a >= 0 and b >= 0  do
     :math.pi() * a * b
   end
+
+  #def area(_, _, _)  do
+  #  0
+  #end
 
 end
